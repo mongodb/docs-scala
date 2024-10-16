2dc11648e1b667206de7e828395ce6327be1d943
Lindsey Moore <lindsey.moore@mongodb.com>
2024-10-16 14:19:12 -0400

    code fix

--- source/includes/connect/ClientExample.scala
+++ source/includes/connect/ClientExample.scala
@@ -12,7 +12,7 @@ object MongoClientConnectionExample {
   def main(args: Array[String]): Unit = {
 
     // Replace the placeholder with your Atlas connection string
-    val connectionString = "mongodb+srv://user:123@atlascluster.spm1ztf.mongodb.net/?authSource=admin";
+    val connectionString = "<connection string>";
 
     // Construct a ServerApi instance using the ServerApi.builder() method
     val serverApi = ServerApi.builder.version(ServerApiVersion.V1).build()
