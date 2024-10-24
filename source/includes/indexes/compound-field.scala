f62305e600d9e3946bcea74b20de60b3bdfe51f9
Lindsey Moore <lindsey.moore@mongodb.com>
2024-10-24 17:55:01 -0400

    DOCSP-42339 Compound Indexes

---
+++ source/includes/indexes/compound-field.scala
@@ -0,0 +1,43 @@
+// start-compound-index-imports
+import org.mongodb.scala._
+import org.mongodb.scala.model.Indexes
+import org.mongodb.scala.model.IndexOptions._
+import org.mongodb.scala.model.Projections._
+import org.mongodb.scala.model.Sorts._
+import org.mongodb.scala.model.Filters._
+// end-compound-index-imports
+
+
+object CompoundFieldIndex {
+
+    def main(args: Array[String]): Unit = {
+
+    // Replace the placeholder with your Atlas connection string
+    val connectionString = "<connection string>";
+
+    // Create a new client and connect to the server
+    val mongoClient = MongoClient(connectionString)
+    val database = mongoClient.getDatabase("sample_mflix")
+    val collection = database.getCollection("movies")
+
+    // start-index-compound
+    collection.createIndex(
+              Indexes.compoundIndex(Indexes.descending("runtime"),
+                                    Indexes.ascending("year")))
+          .printResults()
+
+    // end-index-compound
+
+    // start-index-compound-query
+    val filter = and(gt("runtime", 80), gt("year", 2004))
+
+    collection.find(filter).first().subscribe((doc: Document) => println(doc.toJson()),
+                            (e: Throwable) => println(s"There was an error: $e"))
+    // end-index-compound-query
+
+    // start-check-compound-index
+    collection.listIndexes().printResults()
+    // end-check-compound-index
+
+  }
+}
