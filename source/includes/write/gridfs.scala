95965396f51143255b3875f8fdabe7ce6837a465
Michael Morisi <michael.morisi@mongodb.com>
2024-10-24 13:49:21 -0400

    DOCSP-42326: GridFS

---
+++ source/includes/write/gridfs.scala
@@ -0,0 +1,98 @@
+import org.mongodb.scala._
+import org.mongodb.scala.bson.{Document, ObjectId}
+import org.mongodb.scala.gridfs.GridFSBucket
+import org.mongodb.scala.model._
+
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
+import java.util.concurrent.TimeUnit
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+object GridFS {
+
+  def main(args: Array[String]): Unit = {
+    val mongoClient = MongoClient("<connection string URI>")
+
+    // start-db-coll
+    val database: MongoDatabase = mongoClient.getDatabase("sample_gridfs")
+    // end-db-coll
+
+    // start-create-bucket
+    val bucket = GridFSBucket(database)
+    // end-create-bucket
+
+    // start-create-custom-bucket
+    val filesBucket = GridFSBucket(database, "files")
+    // end-create-custom-bucket
+
+    {
+      // start-upload-files
+      // Get the input stream
+      val observableToUploadFrom = Observable(
+        Seq(ByteBuffer.wrap("MongoDB Tutorial".getBytes(StandardCharsets.UTF_8)))
+      )
+
+      // Create some custom options
+      val options = new GridFSUploadOptions()
+        .chunkSizeBytes(358400)
+        .metadata(Document("type" -> "presentation"))
+
+      // Upload the file
+      val fileIdObservable = bucket.uploadFromObservable("mongodb-tutorial", observableToUploadFrom, options)
+      val fileId = Await.result(fileIdObservable.toFuture(), Duration(10, TimeUnit.SECONDS))
+      println(s"File uploaded with id: ${fileId.toHexString}")
+      // end-upload-files
+    }
+
+    {
+      // start-retrieve-file-info
+      val filesObservable = bucket.find()
+      val results = Await.result(filesObservable.toFuture(), Duration(10, TimeUnit.SECONDS))
+      results.foreach(file => println(s" - ${file.getFilename}"))
+      // end-retrieve-file-info
+    }
+
+    {
+      // start-retrieve-file-info-filter
+      val filesObservable = bucket.find(Filters.equal("metadata.contentType", "image/png"))
+      val results = Await.result(filesObservable.toFuture(), Duration(10, TimeUnit.SECONDS))
+      results.foreach(file => println(s" - ${file.getFilename}"))
+      // end-retrieve-file-info-filter
+    }
+
+    {
+      // start-download-files-name
+      val fileId: ObjectId = new ObjectId()
+      val downloadObservable = bucket.downloadToObservable(fileId)
+      val downloadById = Await.result(downloadObservable.toFuture(), Duration(10, TimeUnit.SECONDS))
+      // end-download-files-name
+    }
+
+    {
+      // start-download-files-id
+      val downloadObservable = bucket.downloadToObservable("mongodb-tutorial")
+      val downloadById = Await.result(downloadObservable.toFuture(), Duration(10, TimeUnit.SECONDS))
+      // end-download-files-id
+    }
+
+    {
+      // start-rename-files
+      val fileId: ObjectId = new ObjectId()
+      val renameObservable = bucket.rename(fileId, "mongodbTutorial")
+      Await.result(renameObservable.toFuture(), Duration(10, TimeUnit.SECONDS))
+      // end-rename-files
+    }
+
+    {
+      // start-delete-files
+      val fileId: ObjectId = new ObjectId()
+      val deleteObservable = bucket.delete(fileId)
+      Await.result(deleteObservable.toFuture(), Duration(10, TimeUnit.SECONDS))
+      // end-delete-files
+    }
+
+    Thread.sleep(1000)
+    mongoClient.close()
+  }
+}
