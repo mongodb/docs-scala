a1e017dda5c1aef4e9a7103aae80fb68ed46268c
Michael Morisi <michael.morisi@mongodb.com>
2024-11-06 14:09:32 -0500

    DOCSP-42341: Atlas Search Indexes

---
+++ source/includes/indexes/atlas-search-index.scala
@@ -0,0 +1,60 @@
+import org.mongodb.scala._
+import org.mongodb.scala.model.SearchIndexModel
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+object StableAPI {
+
+  def main(args: Array[String]): Unit = {
+    val mongoClient = MongoClient("<connection string URI>")
+
+    val database = mongoClient.getDatabase("sample_mflix")
+    val collection = database.getCollection("movies")
+
+    {
+      // start-create-search-index
+      val index = Document("mappings" -> Document("dynamic" -> true))
+      val observable = collection.createSearchIndex("<index name>", index)
+      Await.result(observable.toFuture(), Duration(10, TimeUnit.SECONDS))
+      // end-create-search-index
+    }
+
+    {
+      // start-create-search-indexes
+      val indexOne = SearchIndexModel("<first index name>", Document("mappings" -> Document("dynamic" -> true)))
+      val indexTwo = SearchIndexModel("<second index name>", Document("mappings" -> Document("dynamic" -> true)))
+      val observable = collection.createSearchIndexes(List(indexOne, indexTwo))
+      Await.result(observable.toFuture(), Duration(10, TimeUnit.SECONDS))
+      // end-create-search-indexes
+    }
+
+    {
+      // start-list-search-indexes
+      val observable = collection.listSearchIndexes()
+      observable.subscribe(new Observer[Document] {
+        override def onNext(index: Document): Unit = println(index.toJson())
+        override def onError(e: Throwable): Unit = println("Error: " + e.getMessage)
+        override def onComplete(): Unit = println("Completed")
+      })
+      // end-list-search-indexes
+    }
+
+    {
+      // start-update-search-indexes
+      val updateIndex = Document("mappings" -> Document("dynamic" -> true))
+      val observable = collection.updateSearchIndex("<index to update>", updateIndex)
+      Await.result(observable.toFuture(), Duration(10, TimeUnit.SECONDS))
+      // end-update-search-indexes
+    }
+
+    // start-drop-search-index
+    val observable = collection.dropSearchIndex("<index name>")
+    Await.result(observable.toFuture(), Duration(10, TimeUnit.SECONDS))
+    // end-drop-search-index
+
+    Thread.sleep(1000)
+    mongoClient.close()
+  }
+}
