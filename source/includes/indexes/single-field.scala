a6a9eda43ad4ee2f69759a84643c902539dc85f8
Maya Raman <maya.raman19@gmail.com>
2024-11-18 15:38:04 -0500

    feedback + api doc

--- source/includes/indexes/single-field.scala
+++ source/includes/indexes/single-field.scala
@@ -42,16 +42,6 @@ object SingleFieldIndex {
                             (e: Throwable) => println(s"There was an error: $e"))
     // end-check-single-index
 
-    // start-drop-single-index
-    val observable = collection.dropIndex("<index name>")
-    Await.result(observable.toFuture(), Duration(10, TimeUnit.SECONDS))
-    // end-drop-single-index
-
-    // start-create-geospatial-index
-    val observable = collection.createIndex(Indexes.geo2dsphere("<2d index>"))
-    Await.result(observable.toFuture(), Duration(10, TimeUnit.SECONDS))
-    // end-create-geospatial-index
-
     Thread.sleep(1000)
     mongoClient.close()
   }
